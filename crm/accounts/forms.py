56034c1dc12e1bb535c8b12c1b49cb2ae15055b7
NikolaFloki <ilicalokin@gmail.com>
2022-09-26 20:30:11 +0200

    update

--- crm/accounts/forms.py
+++ crm/accounts/forms.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 from django.forms import ModelForm
 from .models import Order
 
@@ -6,3 +10,9 @@ class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
